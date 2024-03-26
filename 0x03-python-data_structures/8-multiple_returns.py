d07a9a499d043a6c9fe65768830fd7d6ba1d6c56
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-03-26 10:07:21 +0000

    Fixed a bug

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_char = sentence[0]
-    if length > 0:
+    if length == 0:
+        first_char = None
         tup = length, first_char
         return tup
     else:
-        return None
+        length = len(sentence)
+        first_char = sentence[:]
+        tup = length, first_char
+        return tup
