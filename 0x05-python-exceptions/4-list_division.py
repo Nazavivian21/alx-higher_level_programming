8b677732e2ba23aafd9dfe11af687250e90a894e
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-04-03 23:49:58 +0000

    A function that prints an integer

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-	new_list = []
-	try:
-		for i in range(list_length):
-			result = my_list_1[i] / my_list_2[i]
-			if not (isinstance(my_list_1[i], (int, float)) or isinstance(my_list_2[i], (int, float))):
-				print("wrong type")
-			new_list.append(result)
-		return (new_list)
-			# print (new_list)
-	except TypeError:
-		print("wrong type")
-	except ZeroDivisionError:
-		result = 0
-		new_list.append(result)
-		print("division by 0")
-
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            div = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            div = 0
+        except IndexError:
+            print("out of range")
+            div = 0
+        finally:
+            new_list.append(div)
+    return new_list
