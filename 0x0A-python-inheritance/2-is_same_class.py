1d304b23109a449296dd6a4311aecbcd08e00dae
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-04-29 16:35:13 +0000

    Test print_sorted() with no sorted append with negative number and empty list

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+Returns True if the object is an instance of the specified class, otherwise 
+False.
+"""
 def is_same_class(obj, a_class):
     """
-    Returns True if the object is an instance of the specified class, otherwise False.
+    Returns True if the object is an instance of the specified class, otherwise 
+    False.
 
     Args:
             obj: The object to check.
