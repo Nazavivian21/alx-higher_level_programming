86a45bca3fcaee8b1fb542100b7f524b9a52062e
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-04-29 12:37:52 +0000

    Test for an empty list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+Contains MyList class
+"""
+
+
 class MyList(list):
-	"""Inherits from class list."""
-	def print_sorted(self):
-		"""
-		Prints a sorted list
-		
-		Returns:
-			None
-		"""
-		sorted_list = sorted(self)
-		print(sorted_list)
+    """Inherits from class list."""
+
+    def print_sorted(self):
+        """
+        Prints a sorted list
+
+        Returns:
+                None
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
