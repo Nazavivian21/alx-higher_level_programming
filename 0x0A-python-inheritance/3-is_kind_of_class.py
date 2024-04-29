56db531579722d715bb63f9387ce86a61582a208
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-04-29 16:43:01 +0000

    Test print_sorted() with no sorted append with negative number and empty list

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Checks if the object is an instance of the class being inherited from.
+"""
 def is_kind_of_class(obj, a_class):
     """
     Checks if the object is an instance of the class being inherited from.
