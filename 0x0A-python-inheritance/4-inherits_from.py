f166c6f247af26d3620f8d7dfa56065b499aecc1
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-04-30 05:09:13 +0000

    Test for a new list being returned

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
-    """
-    
-    """
-    return isinstance(obj, a_class)
+	issubclass(a_class, obj)
+"""
+while obj_class is not None:
+  if obj_class == a_class:
+		return True
+    obj_class = obj_class.se__
+        
+return False
+"""
