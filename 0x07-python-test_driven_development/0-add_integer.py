184b04c42de15551d90c7967f949a7d1ae2b58f1
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-17 06:04:43 +0100

    Corrected an isinstance syntax error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-	if (not isinstance(a, int) and isinstance(a, float)):
-		raise TypeError('a must be an integer')
-	if (not isinstance(b, int) and isinstance(b, float)):
-		raise TypeError('b must be an integer')
-	
-	if isinstance (a, float):
-		a = int(a)
-	if isinstance (b, float):
-		b = int(b)
-
-	return a + b
+    # Check if 'a' is an integer or a float
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    
+    # Check if 'b' is an integer or a float
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    
+    # Cast 'a' and 'b' to integers (if they are floats)
+    a = int(a)
+    b = int(b)
+    
+    # Return the sum of 'a' and 'b'
+    return a + b
