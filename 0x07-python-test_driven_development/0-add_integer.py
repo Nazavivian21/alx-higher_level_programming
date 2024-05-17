b7a15a7881365cb0a34a8cddb8c434030a092a50
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-17 08:00:59 +0100

    Add module and function documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
+"""Adds two integers"""
+
 def add_integer(a, b=98):
-    # Check if 'a' is an integer or a float
+    """Returns the sum of two numbers"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    # Check if 'b' is an integer or a float
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
