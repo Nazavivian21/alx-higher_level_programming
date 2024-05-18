4aace9356690d0c996c91791aa32557b2c8402d1
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-18 07:17:51 +0100

    Make corrections to the code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,23 +5,25 @@ inserts two new lines after each occurrence of the characters '.', '?',
 and ':'. Without any trailing white space.
 """
 
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     chars = ['.', '?', ':']
-    result = ""
     i = 0
-    while i < len(text):
-        result += text[i]
+    length = len(text)
+
+    while i < length:
+        print(text[i], end="")
         if text[i] in chars:
-            result += "\n\n"
+            print("\n")
+            print()
             i += 1
-            while i < len(text) and text[i] == ' ':
+            while i < length and text[i] == ' ':
                 i += 1
             continue
         i += 1
-
-    print(result.strip())
