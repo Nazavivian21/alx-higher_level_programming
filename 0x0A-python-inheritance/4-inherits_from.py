1013e072025de94215c8f6a02bf6d3fab1f89d03
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-22 06:44:06 +0100

    Add a function that checks for a subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""This module returns True if an object is a sub class"""
 def inherits_from(obj, a_class):
-	issubclass(a_class, obj)
-"""
-while obj_class is not None:
-  if obj_class == a_class:
-		return True
-    obj_class = obj_class.se__
-        
-return False
-"""
+    """
+    Check if an object is an instance of a class that inherited \
+    (directly or indirectly) from the specified class.
+
+    Args:
+    obj: The object to check.
+    a_class: The class to check against.
+
+    Returns:
+    True if the object is an instance of a subclass of a_class, otherwise False.
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
