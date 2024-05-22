592f5ee46c3a5d87d93ad08723244c1dc5f145b1
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-22 06:49:00 +0100

    Add pycodestyle validation

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module returns True if an object is a sub class"""
+
+
 def inherits_from(obj, a_class):
     """
     Check if an object is an instance of a class that inherited \
@@ -8,7 +10,7 @@ def inherits_from(obj, a_class):
     Args:
     obj: The object to check.
     a_class: The class to check against.
-
+    
     Returns:
     True if the object is an instance of a subclass of a_class, otherwise False.
     """
