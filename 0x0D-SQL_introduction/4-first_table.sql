502676d46de4bb6fae8022103cd3bb33feaf4bff
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-24 16:14:27 +0100

    Make coorections to the file

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Creates a table with its description
-CREATE TABLE IF NOT EXISTS mysql.first_table(
+CREATE TABLE IF NOT EXISTS first_table(
 	id INT,
-	name VARCHAR(256),
+	name VARCHAR(256)
 );
