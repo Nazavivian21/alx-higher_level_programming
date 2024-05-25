df20203d443f5ba2ba97ea471d8c7a93f19326bb
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-25 03:30:07 +0100

    Make new corrections to the file

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Shows the description of a table
-SELECT column_name, data_type, character_maximum_length, is_nullable
-FROM information_schema.columns
-WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table
