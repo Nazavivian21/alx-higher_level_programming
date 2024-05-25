aeb788a0de7b2ea3b6d19593f8c5714854b27df4
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-25 13:07:47 +0100

    Add a file that displays specified records in a table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- Inserts a new row to a table
+-- Inserts a new row to a specified table
 INSERT INTO first_table (id, name)
 VALUES (89, 'Best School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Displays records in a table
+SELECT * FROM first_table WHERE id = 89;
