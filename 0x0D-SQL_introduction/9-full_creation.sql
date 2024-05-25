792be2bcef178b0ca47eda4dd6d9e69b4dacc8d2
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-25 13:56:35 +0100

    Make corrections to the file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score INT
 );
 
-INSERT INTO second_table (id, name, score)
+INSERT IGNORE INTO second_table (id, name, score)
 VALUES (1, 'John', 10),
 VALUES (2, 'Alex', 3),
 VALUES (3, 'Bob', 14),
