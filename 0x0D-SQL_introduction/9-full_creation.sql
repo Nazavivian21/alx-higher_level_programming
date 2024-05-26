08daf66c8a84b963bdc71939021b2d17377455d1
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-05-26 03:57:31 +0100

    Make corrections to the file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,9 @@
 --Creates a full table with records
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT,
-	name VARCHAR(256),
-	score INT
-);
-
-INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10);
-VALUES (2, 'Alex', 3);
-VALUES (3, 'Bob', 14);
-VALUES (4, 'George', 8);
+        id INT,
+        name VARCHAR(256),
+        score INT,
+        PRIMARY KEY (id)
+    );
+    INSERT INTO second_table (id, name, score)
+    VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
