3269fd542520881281cdd74065e75b5c71a65608
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-08 19:27:13 +0100

    Correct conversion  to UTF-8 (utf8mb4)

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,19 +1,4 @@
 -- Convert the database to UTF-8 (utf8mb4)
-ALTER DATABASE
-      `hbtn_0c_0`
-      CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
-
-USE `hbtn_0c_0`;
-
-ALTER TABLE
-      `first_table`
-      CONVERT TO CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE
-      `first_table`
-      CHANGE `name`
-      VARCHAR(256)
-      CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE COLUMN name name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
