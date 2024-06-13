d046204e850f8fa49e4fe14e6dc36024e02fb3ae
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-13 15:41:25 +0100

    Fix a subclass bug

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 """Rectangle class definition module"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class BaseGeometry:
-    """Raise exception in area method"""
-
-    def area(self):
-        """Raise exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Check validity of name and value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry"""
