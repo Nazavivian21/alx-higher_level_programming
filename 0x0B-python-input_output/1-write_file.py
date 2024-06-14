b84d69683768cac0aad632b075a4d6480abb5fbf
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-14 13:17:14 +0100

    Make corrections to the documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""The read_file method module"""
+"""The write_file method module"""
 
 
 def write_file(filename="", text=""):
-    """Reads a textfile and prints to stdout:"""
+    """Writes a textfile and prints to stdout:"""
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
