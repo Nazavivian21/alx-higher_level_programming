4b5c7ffd51d361b32a55626aec929a3236f110f7
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-14 12:35:51 +0100

    Make new corrections to the file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""The read_file method module"""
+
+
 def read_file(filename=""):
+    """Reads a textfile and prints to stdout:"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
-
