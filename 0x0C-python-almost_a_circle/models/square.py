298858ddadf6dee3b594dd4d5125250173e174f4
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-15 23:08:32 +0100

    Add pycodestyle validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-from rectangle import Rectangle
+"""The Square class module"""
+from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
     """The Square class that is inheriting from the Rectangle class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes the Square class instance"""
         super().__init__(size, size, x, y, id)
+
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width
         )
-
