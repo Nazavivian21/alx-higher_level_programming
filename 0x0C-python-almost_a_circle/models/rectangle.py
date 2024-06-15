54a04a512e3a478b63971f295d3a5f18fdd676ba
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-15 19:48:40 +0100

    Add a new pycodestyle validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,5 +73,6 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return a string representation of the rectangle."""
-        return f"[Rectangle]({self.id}) {self.x}/{self.y} - \
-            {self.width}/{self.height}"
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height
+        )
