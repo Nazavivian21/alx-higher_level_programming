7fbcf42e6264c7144176dd52b0453ab1e746d597
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-15 19:11:31 +0100

    Add the display method to the rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -65,3 +65,8 @@ class Rectangle(Base):
     def area(self):
         """Calculate the area of the rectangle."""
         return self.width * self.height
+
+    def display(self):
+        """Print the rectangle using the '#' character."""
+        for _ in range(self.height):
+            print("#" * self.width)
