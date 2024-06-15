d52dacd446ccf12cbc38ba17ee864e55c65a124b
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-15 19:21:04 +0100

    Add the __str__  method to the rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,3 +70,7 @@ class Rectangle(Base):
         """Print the rectangle using the '#' character."""
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        """Return a string representation of the rectangle."""
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
