93aec617eca21676f35da7f6f03452250236a459
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-19 02:37:10 +0100

    Make corrections to the file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
+const size = parseInt(args[0], 10);
 
-const x = parseInt(args[0], 10);
-
-for (let i = 0; i < x || isNaN(x); i++) {
-  if (isNaN(x)) {
-    console.log('Missing number of occurrences');
-    break;
-  } else {
-    console.log('C is fun');
+if (isNaN(size)) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let j = 0; j < size; j++) {
+      line += 'X';
+    }
+    console.log(line);
   }
 }
