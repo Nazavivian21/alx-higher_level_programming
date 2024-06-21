69aa0017b0fcd9036c12fc09dc0f501a7f16d3f4
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-21 17:43:33 +0100

    Add two instance methods rotate and double to the Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+      return this.object;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    if (this.width && this.height) {
+      [this.width, this.height] = [this.height, this.width];
+    }
+  }
+
+  double () {
+    if (this.width && this.height) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+
+module.exports = Rectangle;
