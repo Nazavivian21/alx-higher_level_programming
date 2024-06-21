02c451fb629e076820f38c9b91abd25cb150a63b
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-21 17:24:46 +0100

    Add an instance method that prints the rectangle using the character X

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+      return this.object;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Instance method to print the rectangle
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    } else {
+      console.log('Invalid rectangle dimensions');
+    }
+  }
+}
+
+module.exports = Rectangle;
