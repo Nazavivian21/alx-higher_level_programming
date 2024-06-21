5be93d5785d9c4360b42bed0c7fc48b41c19c84f
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-21 02:44:49 +0100

    Make corrections to the file

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      return {};
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+            return {};
+        }
+        this.width = w;
+        this.height = h;
     }
-  }
 }
 module.exports = Rectangle;
