11a4faa56f75e209cd6520e342fcec7401036908
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-21 16:57:10 +0100

    Make corrections to the file

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
-      // Create an empty object
+
       return this.object;
     } else {
       this.width = w;
