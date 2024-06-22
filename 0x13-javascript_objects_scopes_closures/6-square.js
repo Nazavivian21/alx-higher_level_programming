974c693f5dd86114805bdaf718c807d5c1fbee85
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-22 04:39:16 +0100

    Make corrections to the file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const baseSquare = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends baseSquare {
   constructor (size) {
     super(size, size);
   }
