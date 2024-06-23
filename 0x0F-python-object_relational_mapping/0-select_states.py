1f9a27f8b0522aec044483d5a14457769f6c28b0
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-06-23 22:02:54 +0100

    Make corrections to the file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name,
+        user='root',
+        passwd='',
+        db='hbtn_0e_0_usa',
     )
 
     cur = db.cursor()
