e6d606f0d492afd411aa3245d531714516d19405
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-07-07 23:33:52 +0100

    Make new corrections to the file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Sends a request to a URL and displays the size of the body of the response.
-response_size=$(curl -s -o /dev/null -w '%{size_download}' "$1")
-echo "The size of the body of the response is $response_size bytes"
+curl -s "$1" | wc -c 
