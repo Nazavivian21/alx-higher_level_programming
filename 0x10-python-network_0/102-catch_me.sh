a54a7dae5c3adc60cc9ba4b02006761c545a3091
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-07-09 04:10:14 +0100

    Make corrections to the file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make the request and display the response body
-curl -sL -X PUT -H  "Origin: Holberton School" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -X PUT -d "user_id=42" -H "Origin: School" 0.0.0.0:5000/catch_me
