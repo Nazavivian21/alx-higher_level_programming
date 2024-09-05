7492a6adae391554962c06f8c211182a6f65ea99
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-09-05 12:12:24 +0100

    Takes in a URL and an email, sends a POST request to a passed URL

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-import urllib.parse
-import urllib.request
-import sys
+if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('utf-8')
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
 
-req = urllib.request.Request(url, data)
+    req = urllib.request.Request(url, data)
 
-with urllib.request.urlopen(req) as response:
-  # Decode response body
-  body = response.read().decode('utf-8')
-  # Print the response body
-  print(body)
+    with urllib.request.urlopen(req) as response:
+        # Decode response body
+        body = response.read().decode('utf-8')
+        # Print the response body
+        print(body)
