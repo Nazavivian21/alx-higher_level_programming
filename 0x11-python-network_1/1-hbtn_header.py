04671baa41a3a2c4c75caf3e8b6e97419497ba2c
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-09-05 10:41:04 +0100

    Document Module

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """A python file that takes in a URL and sends request to a URL"""
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-import urllib.request
-import sys
+    url = sys.argv[1]
 
-url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        headers = response.headers
 
-with urllib.request.urlopen(url) as response:
-    headers = response.headers
-
-x_request_id = headers.get('X-Request-Id')
-print(x_request_id)
+    x_request_id = headers.get('X-Request-Id')
+    print(x_request_id)
