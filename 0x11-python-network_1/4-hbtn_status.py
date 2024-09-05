d1e6fe69bf3e5e61ff07d616123a1bf5193cff8f
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-09-05 13:07:36 +0100

    Fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches a URL and displays the body of the response."""
+import requests
+
+if __name__ == "__main__":
+    # Fetch the URL
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    
+    # Display the response body in the required format
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
