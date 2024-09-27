ff5ca4c47900e3aca217ec28a3839c740fc900ff
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-09-27 21:52:50 +0100

    Add semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,35 @@
 #!/usr/bin/node
-const request = require('request');
-const apiUrl = process.argv[2]; // Get the API URL from the first argument
-const wedgeAntillesId = '18'; // Wedge Antilles character ID
 
-// Make a GET request to the Star Wars API
+const request = require('request');
+
+// API URL for Star Wars films
+const apiUrl = process.argv[2] || 'https://swapi-api.alx-tools.com/api/films/';
+
+// Character ID for Wedge Antilles
+const wedgeAntillesID = 18;
+
+// Make a request to the API
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error('Error:', error); // Print the error if one occurred
-  } else {
-    const films = JSON.parse(body).results; // Parse the API response and get the films
-    let count = 0;
-
-    // Loop through each film and check if Wedge Antilles (ID 18) is in the character list
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
-        count++;
-      }
-    });
-
-    // Print the number of movies where Wedge Antilles is present
-    console.log(count);
+    console.error(error);
+    return;
   }
+
+  // Parse the JSON response
+  const films = JSON.parse(body);
+  let count = 0;
+
+  // Count the number of films that include Wedge Antilles
+  films.results.forEach(film => {
+    if (film.characters) {
+      film.characters.forEach(character => {
+        if (character.includes(`/${wedgeAntillesID}/`)) {
+          count++;
+        }
+      });
+    }
+  });
+
+  // Print the result
+  console.log(count);
 });
