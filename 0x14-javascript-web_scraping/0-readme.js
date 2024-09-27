23fff57f287ce0502ef602e02eefa9d7edfde55e
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-09-27 19:48:07 +0100

    reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];  // Get the file path from the first argument
-
+const filePath = process.argv[2]; // Get the file path from the first argument
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        // If an error occurred, print the error object
-        console.error(err);
-    } else {
-        // Print the file content
-        console.log(data);
-    }
+  if (err) {
+    // If an error occurred, print the error object
+    console.error(err);
+  } else {
+    // Print the file content
+    console.log(data);
+  }
 });
