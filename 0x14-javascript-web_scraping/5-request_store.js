210f2faeb56bff01a8cbe023f3f306fb75159b19
Nazavivian21 <nazafavourukwe18@gmail.com>
2024-09-27 22:00:55 +0100

    Add semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2]; // Get the URL from the first argument
-const filePath = process.argv[3]; // Get the file path from the second argument
+// Get the URL and file path from command-line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
 
-// Make a GET request to the provided URL
+// Make a request to the provided URL
 request(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error); // Print the error if one occurred
-  } else {
-    // Write the response body to the specified file in UTF-8 encoding
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error('Error writing to file:', err); // Print error if unable to write
-      } else {
-        console.log('File written successfully!');
-      }
-    });
+    console.error(error);
+    return;
   }
+
+  // Write the response body to the specified file
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
